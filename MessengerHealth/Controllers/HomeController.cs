4c24eca01e8f027eff57e9b738a8a97d03cf9cd6
PauloHuertas <paulo.huertas@expeditors.com>
2024-01-04 14:43:35 +0000

    Add project files.

---
+++ MessengerHealth/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using MessengerHealth.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessengerHealth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string dir = ConfigurationManager.AppSettings["directoryIn"];
+
+            string[] files = Directory.GetFiles(dir);
+            MessengerFiles.lastWrittenDate = Directory.GetLastWriteTime(dir);
+            List<MessengerFiles> messengerFiles1 = new List<MessengerFiles>();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                DateTime lastTime = Directory.GetLastAccessTimeUtc(files[i]);
+                string fileName = files[i];
+                MessengerFiles messengerFiles = new MessengerFiles(fileName, lastTime);
+                messengerFiles1.Add(messengerFiles);
+            }
+
+            int numberOfFiles = Directory.GetFiles(dir).Length;
+
+            return View(messengerFiles1);
+        }
+        [HttpPost]
+        public ActionResult Index(string jurisdiction, string procedureCode, string service)
+        {
+            string keySearch = $@"directory{service}";
+            string dir = String.Empty;
+
+            if (!String.IsNullOrEmpty(keySearch))
+            {
+                dir = ConfigurationManager.AppSettings[keySearch];
+            }
+            else
+            {
+                dir = ConfigurationManager.AppSettings["directoryIn"];
+            }
+            
+            if (!String.IsNullOrEmpty(dir)) dir += $@"{jurisdiction}\{procedureCode}";
+
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            string[] files = Directory.GetFiles(dir);
+
+            MessengerFiles.lastWrittenDate = Directory.GetLastWriteTime(dir);
+
+            List<MessengerFiles> messengerFiles1 = new List<MessengerFiles>();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                DateTime lastTime = Directory.GetLastAccessTimeUtc(files[i]);
+                string fileName = files[i];
+                MessengerFiles messengerFiles = new MessengerFiles(fileName, lastTime);
+                messengerFiles1.Add(messengerFiles);
+            }
+
+            int numberOfFiles = files.Length;
+
+            return View(messengerFiles1);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MessengerHealth/Models/MessengerFiles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessengerHealth.Models
+{
+    public class MessengerFiles
+    {
+        public string FileName;
+        public DateTime LasTimeAccessed;
+        public static DateTime lastWrittenDate;
+
+        public MessengerFiles(string file, DateTime lastTimeAccessed)
+        {
+            this.FileName = file;
+            this.LasTimeAccessed = lastTimeAccessed;
+        }
+
+        public string GetShortFileName()
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(this.FileName)) return "";
+
+                string temp = String.Empty;
+
+                temp = this.FileName;
+
+                temp = temp.Substring(this.FileName.LastIndexOf("\\") + 1);
+
+                return temp;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}

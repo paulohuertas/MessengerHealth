ce515122ebd0d3d66e3c614cc3940245b20d6378
Paulo Victor Huertas <paulohuertas@outlook.com>
2024-01-15 00:47:13 +0000

    pie chart changes

--- MessengerHealth/Controllers/HomeController.cs
+++ MessengerHealth/Controllers/HomeController.cs
@@ -29,11 +29,103 @@ namespace MessengerHealth.Controllers
                 MessengerFiles messengerFiles = new MessengerFiles(fileName, lastTime);
                 messengerFiles1.Add(messengerFiles);
             }
-            string path = @"C:\Users\dub-pauloh\Desktop\PauloExpeditors\Personal\C#\MessengerHealth\MessengerHealth\MessengerHealth\directories.json";
-            ReadAndParseJsonFile parser = new ReadAndParseJsonFile(path);
-            List<Juridisction> juridisctions = parser.UseUserDefinedObjectJson();
 
-            return View(messengerFiles1);
+            string path = @"C:\01. Paulo Huertas\c#\Messenger\MessengerHealth\MessengerHealth\directories_local.json";
+            ReadAndParseJsonFile parser = new ReadAndParseJsonFile(path);
+            List<Jurisdiction> juridisctions = parser.UseUserDefinedObjectJson();
+
+            Dictionary<string, Dictionary<string, List<string>>> proceduresServiceDirectoryIn = new Dictionary<string, Dictionary<string, List<string>>>();
+            Dictionary<string, Dictionary<string, List<string>>> proceduresServiceDirectoryOut = new Dictionary<string, Dictionary<string, List<string>>>();
+            Dictionary<string, List<string>> serviceDirectoryIn = new Dictionary<string, List<string>>();
+            Dictionary<string, List<string>> serviceDirectoryOut = new Dictionary<string, List<string>>();
+            List<string> inDirectories = new List<string>();
+            
+            List<string> outDirectories = new List<string>();
+            for (int i = 0; i < juridisctions.Count; i++)
+            {
+                string keyCode = juridisctions[i].JuridisctionCode;
+
+                foreach (var inPath in juridisctions[i].Procedures.Services.In)
+                {
+                    string pathDir = inPath.DirectoryPath;
+                    inDirectories.Add(pathDir);
+
+                    if (serviceDirectoryIn.ContainsKey(keyCode))
+                    {
+                        serviceDirectoryIn[keyCode] = inDirectories.ToList();
+                    }
+                    else
+                    {
+                        serviceDirectoryIn.Add(keyCode, inDirectories.ToList());
+                    }
+                }
+
+                inDirectories.Clear();
+
+                keyCode = juridisctions[i].JuridisctionCode;
+
+                foreach (var outPath in juridisctions[i].Procedures.Services.Out)
+                {
+                    string pathDir = outPath.DirectoryPath;
+                    outDirectories.Add(pathDir);
+
+                    if (serviceDirectoryOut.ContainsKey(keyCode))
+                    {
+                        serviceDirectoryOut[keyCode] = outDirectories.ToList();
+                    }
+                    else
+                    {
+                        serviceDirectoryOut.Add(keyCode, outDirectories.ToList());
+                    }
+                }
+
+                outDirectories.Clear();
+            }
+
+            Dictionary<string, Dictionary<string, List<string>>> keyValuePairs = new Dictionary<string, Dictionary<string, List<string>>>();
+            keyValuePairs.Add("In", serviceDirectoryIn);
+            keyValuePairs.Add("Out", serviceDirectoryOut);
+
+            List<string> keys = new List<string>(keyValuePairs.Keys);
+            Dictionary<string, int> inTotalNumberFiles = new Dictionary<string, int>();
+
+            int counter = 0;
+            foreach(var dict in keyValuePairs)
+            {
+                string currentKey = keys[counter];
+
+                Dictionary<string, List<string>> values = keyValuePairs[currentKey];
+
+                List<string> procedureKeys = new List<string>(values.Keys);
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    string procedureKey = procedureKeys[i];
+
+                    for(int j = 0; j < values[procedureKey].Count; j++)
+                    {
+                        string currentPath = values[procedureKey][j];
+                        if (!Directory.Exists(currentPath))
+                        {
+                            Directory.CreateDirectory(currentPath);
+                        }
+
+                        int numberOfFilesInDirectory = Directory.GetFiles(currentPath).Length;
+                        if (numberOfFilesInDirectory > 0)
+                        {
+                            inTotalNumberFiles.Add(currentPath, numberOfFilesInDirectory);
+                        }
+                    }
+                    
+                }
+                counter++;
+            }
+
+            List<Dictionary<string, int>> list = new List<Dictionary<string, int>>();
+            list.Add(inTotalNumberFiles);
+
+            //return View(messengerFiles1);
+            return View(list);
         }
         [HttpPost]
         public ActionResult Index(string jurisdiction, string procedureCode, string service)
---
+++ MessengerHealth/Models/Main.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MessengerHealth.Models
+{
+    public class In
+    {
+        [JsonProperty("procedureCode")]
+        public string ProcedureCode { get; set; }
+
+        [JsonProperty("directoryPath")]
+        public string DirectoryPath { get; set; }
+    }
+
+    public class Out
+    {
+        [JsonProperty("procedureCode")]
+        public string ProcedureCode { get; set; }
+
+        [JsonProperty("directoryPath")]
+        public string DirectoryPath { get; set; }
+    }
+
+    public class Procedures
+    {
+        [JsonProperty("services")]
+        public Services Services { get; set; }
+    }
+
+    public class Jurisdiction
+    {
+        [JsonProperty("jurisdictionId")]
+        public int JurisdictionId { get; set; }
+
+        [JsonProperty("juridisctionCode")]
+        public string JuridisctionCode { get; set; }
+
+        [JsonProperty("procedures")]
+        public Procedures Procedures { get; set; }
+    }
+
+    public class Services
+    {
+        [JsonProperty("Out")]
+        public List<Out> Out { get; set; }
+
+        [JsonProperty("In")]
+        public List<In> In { get; set; }
+    }
+
+    public class ReadAndParseJsonFile
+    {
+        private readonly string _sampleJsonFile;
+        public ReadAndParseJsonFile(string jsonFile)
+        {
+            _sampleJsonFile = jsonFile;
+        }
+
+        public List<Jurisdiction> UseUserDefinedObjectJson()
+        {
+            using (StreamReader streamReader = new StreamReader(_sampleJsonFile))
+            {
+                var json = streamReader.ReadToEnd();
+                List<Jurisdiction> juridisctions = JsonConvert.DeserializeObject<List<Jurisdiction>>(json);
+                return juridisctions;
+            }
+        }
+    }
+}
